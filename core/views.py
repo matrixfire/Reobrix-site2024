0547bfa6d0e7b65034d9778832e7b73f25a227ab
matrixfire <matrixbox@qq.com>
2024-05-29 11:28:31 +0800

    k

---
+++ core/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    """The home page for reobrix."""
+    return render(request, 'core/index.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+
+
+
+def programs(request):
+    return render(request, 'core/programs.html')
+
+def gallery(request):
+    return render(request, 'core/gallery.html')
+
+def faq(request):
+    return render(request, 'core/faq.html')
+
+def reviews(request):
+    return render(request, 'core/reviews.html')
+
+def contacts(request):
+    return render(request, 'core/contacts.html')
+
+
+def search_results(request):
+    return render(request, 'core/search-results.html')
+
+def single_program(request):
+    return render(request, 'core/single-program.html')
