35fb0457a43da6757ad38f6622499d88726aeb7a
matrixfire <matrixbox@qq.com>
2024-05-31 18:25:08 +0800

    5.31 OK

--- blog/views.py
+++ blog/views.py
@@ -50,11 +50,18 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day)
 
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    # Form for users to comment
+    form = CommentForm()
     
     return render(
         request,
         'blog/post/detail.html',
-        {'post': post}
+        {'post': post,
+         'comments': comments,
+         'form': form
+         }
     )
 
 
